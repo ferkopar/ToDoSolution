75859d2ed1610ff31a8798ee17da1bbbbb29d148
Párdi Ferenc <ferkopar@gmail.com>
2015-12-30 19:56:34 +0100

    add apex scripts

---
+++ ToDoWebApp/Scripts/apex/P11300.js
@@ -0,0 +1,53 @@
+﻿function openTree(p, id) {
+   // alert(id);
+   // var vItem = $(p).next("span");
+   // var vLi = $(p).closest("li");
+   // var outerSpan = $(p).parent();
+   // var position = outerSpan.position();
+   // var spanTxt = '<span style="display:block;position: relative;left:' + position.left + 'px; top:7px;"> Ez az új tartalom </span>';
+   // var get = new htmldb_Get(null,&APP_ID.,'APPLICATION_PROCESS=getSubTree',11300);
+   // get.addParam("x01",id); 
+   // var gReturn = get.get();
+   //// alert(gReturn);
+   // get = null;
+   // vItem.append(gReturn);
+   // var table = $(p).next("span").children("table");
+   // vLi.height(vLi.height() + table.height());
+    // outerSpan.height(outerSpan.height() + table.height());
+    window.location.replace("f?p=" + $v('pFlowId') + ":11200:" + $v('pInstance') + '::::P11200_ROOT:' + id);
+
+}
+
+function LoadTree(p) {
+    var typeId = $(p).data('typid');
+    //alert(typeId);   
+     var get = new htmldb_Get(null,&APP_ID.,'APPLICATION_PROCESS=getSubTree',11300);
+     get.addParam("x01",typeId); 
+     get.GetAsync(function(pResponse){
+         console.log(pResponse.readyState);
+         if(pResponse.readyState==4 && pResponse.status==200){
+             $("#SuperTypeTable > tbody").html(pResponse.responseText);
+         };
+     });
+     //var gReturn = get.get();
+     //alert(gReturn);
+    $("#SuperTypeTable > tbody").empty();
+    //$("#SuperTypeTable > tbody").html(gReturn);
+}
+
+function GoToEditor(p) {
+    var typeId = $(p).data('typeid');
+    
+    var urn = 'f?p=&APP_ID.:11301:&SESSION.::NO::P11301_SUPER_TYP_ID:' + typeId;
+    //f?p=App:Page:Session:Request:Debug:ClearCache:itemNames:itemValues:PrinterFriendly
+    window.location = urn;
+}
+
+function addDescendant(p) {
+    var e = window.event;
+    e.stopPropagation();
+    alert("addDescendant");
+    var urn = "f?p=&APP_ID.:11301:&SESSION.:ADD:NO::P11301_SUPER_TYP_ID:" + $(p).data("id");
+    //f?p=App:Page:Session:Request:Debug:ClearCache:itemNames:itemValues:PrinterFriendly
+    window.location = urn;
+}
---
+++ ToDoWebApp/Scripts/apex/P3000.js
@@ -0,0 +1,113 @@
+﻿
+$(document).ready(function () {
+    setInputElementsWidth();
+});
+
+
+function setInputElementsWidth() {
+    $("#mainTable input").each(function () {
+        $(this).width($(this).closest('th').width());
+        $(this).css("padding", "5px");
+    });
+    $("#mainTable select").each(function () {
+        if ($(this).siblings().size() > 0) {
+            $(this).width($(this).closest('th').width() - 32);
+        } else {
+            $(this).width($(this).closest('th').width());
+        }
+        $(this).css("padding", "5px");
+    });
+}
+
+function goToEdit(p) {
+
+    var urn;
+    if(p==0)
+        urn = 'f?p=&APP_ID.:3001:&SESSION.::NO:3001:P3001_SUBJ_CATEGORY_ID:'+$v("P3000_SUBJ_TYPE");
+    else
+        urn = 'f?p=&APP_ID.:3001:&SESSION.::NO::P3001_SUBJ_ID,P3001_SUBJ_CATEGORY_ID:' + p +','+$v("P3000_SUBJ_TYPE");
+
+    //f?p=App:Page:Session:Request:Debug:ClearCache:itemNames:itemValues:PrinterFriendly
+    alert(urn);
+    window.location = urn;
+}
+
+function changeOrder(o) {
+    var workspaceImages = "#WORKSPACE_IMAGES#";
+    var data = $(o).data("ord");
+    if (data == "none") {
+        o.src = workspaceImages + "sort-alphabet-icon.png";
+        $(o).data("ord", "asc");
+    }
+    else if (data == "asc") {
+        o.src = workspaceImages + "sort-alphabet-descending-icon.png";
+        $(o).data("ord","desc");
+    }
+    else {
+        o.src = workspaceImages + "sort-icon.png";
+        $(o).data("ord", "none");
+    }
+
+    deleteDataRows();
+    reloadMainTable(o);
+}
+
+function changeSearch(o) {
+    deleteDataRows();
+    reloadMainTable(o);
+}
+
+function deleteDataRows() {
+    var dataRows = $("#mainTable .dataRow");
+    dataRows.each(function () {
+        $(this).remove();
+    });
+}
+
+function reloadMainTable(p) {
+    var whereExists = false;
+    var jsonTxt = "{\n";
+    jsonTxt += formatJsonKeyValuePairNoComma("SUBJ_TYPE", $v("P3000_SUBJ_TYPE"));
+
+    var whereTxt = "";
+    var t = $("#srcTYPE").val();
+    if (!(t == 0)) {
+        whereExists = true;
+        whereTxt += t;
+    }
+    var sortTxt = "";
+    var s = $(".sortImg");
+    s.each(
+        function(){
+            var $this = $(this);
+            if($this.data("ord")!="none"){
+                if($this.data("ord")!="asc"){
+                    sortTxt += $this.data("col")+" desc,";
+                }
+                else{
+                    sortTxt += $this.data("col")+",";
+                }
+               
+            }
+        }
+    );
+    if (whereExists) {
+
+        jsonTxt += ","+formatJsonKeyValuePairNoCommaStr("WHERE", whereTxt);
+    }
+
+    if (! (sortTxt === "")) {
+        sortTxt = sortTxt.substring(0, sortTxt.length - 1);
+        jsonTxt += ",\n";
+        jsonTxt += formatJsonKeyValuePairNoCommaStr("ORDER", sortTxt);
+    }
+    jsonTxt += "}\n";
+    var dataTable = $("#mainTable");
+    $s("P3000_TESZT",jsonTxt); 
+    var get = new htmldb_Get(null,&APP_ID.,'APPLICATION_PROCESS=reload',3000);
+    get.addParam("x01", jsonTxt);
+    //get.addParam("x02", selected);    
+    var retVal = get.get(); 
+    $s("P3000_TESZT",jsonTxt+"\n"+retVal);  
+    dataTable.append(retVal);
+}
